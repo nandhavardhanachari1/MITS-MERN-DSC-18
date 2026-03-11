ab611047924e9c71aa2d297d1e5fa6f6e3fba059
nandhavardhanachari1 <nandhavardhanachari@gmail.com>
2026-03-11 11:31:12 +0530

    mern project

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -1,11 +1,12 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const connectDB = async()=>{
-    try{
-        await mongoose.connect(process.env.MONGO_URL);
-        console.log("MongoDB Connected Successfully🫳🎉")
-    }catch(err){
-        console.log(err);
-    }
-} 
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("MongoDB Connected");
+  } catch (error) {
+    console.log(error);
+  }
+};
+
 module.exports = connectDB;
